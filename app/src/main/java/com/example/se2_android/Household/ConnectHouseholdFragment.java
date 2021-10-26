1086d357359632784283d7101f89ef7f3d028522
ValterSj <valter.sjo@gmail.com>
2021-10-26 13:15:54 +0200

    join household!

--- app/src/main/java/com/example/se2_android/Household/ConnectHouseholdFragment.java
+++ app/src/main/java/com/example/se2_android/Household/ConnectHouseholdFragment.java
@@ -1,6 +1,7 @@
 package com.example.se2_android.Household;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -42,11 +44,55 @@ public class ConnectHouseholdFragment extends Fragment {
         joinHouseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+                dialog.setTitle("Join Household");
 
+                EditText enterID = new EditText(getContext());
+                enterID.setInputType(InputType.TYPE_CLASS_NUMBER);
+                enterID.setHint("Enter id of household");
+                dialog.setView(enterID);
+
+                dialog.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String id = enterID.getText().toString();
+                        checkIdSendToServer(id);
+                    }
+                });
+
+
+                dialog.setNegativeButton("Go back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.setNeutralButton("Help", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(), "Enter the id of the household you want to join!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.show();
             }
         });
 
         return view;
+    }
+
+
+    public void checkIdSendToServer(String id){
+        if (!id.matches("[0-9]+")){
+            Toast.makeText(getContext(), "A valid ID is required!", Toast.LENGTH_SHORT).show();
+        } else {
+            householdStub.setHouseholdID(id);
+            householdStub.setHouseholdName("Id set to - " + id);
+            Toast.makeText(getContext(), "Joined Household with id - " + householdStub.getHouseholdID(), Toast.LENGTH_SHORT).show();
+            Navigation.findNavController(view).navigate(R.id.action_connectHouseholdFragment_to_homeFragment);
+        }
+
     }
 
 
